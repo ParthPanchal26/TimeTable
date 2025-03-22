57d43cb6ea957feacaa61099dd671938132cf649
ParthPanchal26 <parth.code2625@gmail.com>
2025-03-22 14:30:29 +0530

    Created a complete Time Table viewer web

---
+++ constants.js
@@ -0,0 +1,93 @@
+export const timeTable = {
+  "timetable": [
+    {
+      "id": 0,
+      "day": "Sunday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "Holiday" },
+        { "time": "10:00 - 10:50", "subject": "Holiday" },
+        { "time": "10:50 - 11:45", "subject": "Holiday" },
+        { "time": "11:50 - 12:40", "subject": "Holiday" },
+        { "time": "12:40 - 01:25", "subject": "Holiday" },
+        { "time": "01:25 - 02:15", "subject": "Holiday" },
+        { "time": "02:20 - 03:10", "subject": "Holiday" },
+      ]
+    },
+    {
+      "id": 1,
+      "day": "Monday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "Free" },
+        { "time": "10:00 - 10:50", "subject": "Software Engineering" },
+        { "time": "10:50 - 11:45", "subject": "Artificial Intelligence and Machine Learning" },
+        { "time": "11:50 - 12:40", "subject": "Microprocessor Technology" },
+        { "time": "12:40 - 01:25", "subject": "Lunch Break" },
+        { "time": "01:25 - 02:15", "subject": "MP Lab" },
+        { "time": "02:20 - 03:10", "subject": "MP Lab" },
+      ]
+    },
+    {
+      "id": 2,
+      "day": "Tuesday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "Microprocessor Technology" },
+        { "time": "10:00 - 10:50", "subject": "Artificial Intelligence and Machine Learning" },
+        { "time": "10:50 - 11:45", "subject": "MP Lab" },
+        { "time": "11:50 - 12:40", "subject": "MP Lab" },
+        { "time": "12:40 - 01:25", "subject": "Lunch Break" },
+        { "time": "02:20 - 03:10", "subject": "MT Lab" },
+        { "time": "01:25 - 02:15", "subject": "MT Lab" },
+      ]
+    },
+    {
+      "id": 3,
+      "day": "Wednesday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "Software Engineering" },
+        { "time": "10:00 - 10:50", "subject": "Microprocessor Technology" },
+        { "time": "10:50 - 11:45", "subject": "Cyber Security" },
+        { "time": "11:50 - 12:40", "subject": "Supply Chain Management" },
+        { "time": "12:40 - 01:25", "subject": "Lunch Break" },
+        { "time": "02:20 - 03:10", "subject": "SE Lab" },
+        { "time": "01:25 - 02:15", "subject": "SE Lab" },
+      ]
+    },
+    {
+      "id": 4,
+      "day": "Thursday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "Software Engineering" },
+        { "time": "10:00 - 10:50", "subject": "Artificial Intelligence and Machine Learning" },
+        { "time": "10:50 - 11:45", "subject": "Cyber Security" },
+        { "time": "11:50 - 12:40", "subject": "Supply Chain Management" },
+        { "time": "12:40 - 01:25", "subject": "Lunch Break" },
+        { "time": "01:25 - 02:15", "subject": "AIML Lab" },
+        { "time": "02:20 - 03:10", "subject": "AIML Lab" },
+      ]
+    },
+    {
+      "id": 5,
+      "day": "Friday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "CYS Lab" },
+        { "time": "10:00 - 10:50", "subject": "CYS Lab" },
+        { "time": "10:50 - 11:45", "subject": "Cyber Security" },
+        { "time": "11:50 - 12:40", "subject": "Supply Chain Management" },
+        { "time": "12:40 - 01:25", "subject": "Lunch Break" },
+      ]
+    },
+    {
+      "id": 6,
+      "day": "Saturday",
+      "schedule": [
+        { "time": "09:00 - 10:00", "subject": "Holiday" },
+        { "time": "10:00 - 10:50", "subject": "Holiday" },
+        { "time": "10:50 - 11:45", "subject": "Holiday" },
+        { "time": "11:50 - 12:40", "subject": "Holiday" },
+        { "time": "12:40 - 01:25", "subject": "Holiday" },
+        { "time": "01:25 - 02:15", "subject": "Holiday" },
+        { "time": "02:20 - 03:10", "subject": "Holiday" },
+      ]
+    }
+  ]
+}
